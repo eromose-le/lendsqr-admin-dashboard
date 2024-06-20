ba2d8e2bfbb0213b774e24cf87a250aff057abf0
eromose-le <erons24@yahoo.com>
2024-06-20 21:06:05 +0100

    chore: add icon pack

---
+++ src/assets/constants/assets.ts
@@ -0,0 +1,104 @@
+import logo from '../images/logo.svg';
+
+import signin_image from '../images/signin_image.svg';
+
+import search from '../images/search.svg';
+import notification from '../images/notification.svg';
+import down_arrow from '../images/down_arrow.svg';
+import cancel from '../images/cancel.svg';
+
+import dashboard from '../images/dashboard.svg';
+import switch_icon from '../images/dashboard/switch-icon.svg';
+import user_friends from '../images/dashboard/user-friends.svg';
+import guarantors from '../images/dashboard/guarantors.svg';
+import loan from '../images/dashboard/loan.svg';
+import decision from '../images/dashboard/decision.svg';
+import piggy from '../images/dashboard/piggy.svg';
+import loan_request from '../images/dashboard/loan-request.svg';
+import whitelist from '../images/dashboard/whitelist.svg';
+import karma from '../images/dashboard/karma.svg';
+import saving from '../images/dashboard/saving.svg';
+import coins_solid from '../images/dashboard/coins-solid.svg';
+import transaction from '../images/dashboard/transaction.svg';
+import services from '../images/dashboard/services.svg';
+import service_account from '../images/dashboard/service-account.svg';
+import settlements from '../images/dashboard/settlements.svg';
+import report from '../images/dashboard/report.svg';
+import preferences from '../images/dashboard/preferences.svg';
+import fees from '../images/dashboard/fees.svg';
+import audit from '../images/dashboard/audit.svg';
+import sign_out from '../images/dashboard/sign-out.svg';
+
+import back from '../images/user/back.svg';
+import avatar from '../images/user/avatar.svg';
+
+import activate from '../images/overview/activate.svg';
+import blacklist from '../images/overview/blacklist.svg';
+import calender from '../images/overview/calender.svg';
+import dot from '../images/overview/dot.svg';
+import down from '../images/overview/down.svg';
+import filter from '../images/overview/filter.svg';
+import left from '../images/overview/left.svg';
+import loans from '../images/overview/loans.svg';
+import right from '../images/overview/right.svg';
+import savings from '../images/overview/savings.svg';
+import user from '../images/overview/user.svg';
+import users from '../images/overview/users.svg';
+import view from '../images/overview/view.svg';
+
+import loading from '../images/loading.gif';
+import giphy from '../images/giphy.gif';
+
+const assets = {
+  logo,
+
+  signin_image,
+
+  search,
+  notification,
+  down_arrow,
+  cancel,
+
+  dashboard,
+  switch_icon,
+  user_friends,
+  guarantors,
+  loan,
+  decision,
+  piggy,
+  loan_request,
+  whitelist,
+  karma,
+  saving,
+  coins_solid,
+  transaction,
+  services,
+  service_account,
+  settlements,
+  report,
+  preferences,
+  fees,
+  audit,
+  sign_out,
+
+  back,
+  avatar,
+
+  activate,
+  blacklist,
+  calender,
+  dot,
+  down,
+  filter,
+  left,
+  loans,
+  right,
+  savings,
+  user,
+  users,
+  view,
+
+  giphy,
+  loading
+};
+export default assets;
